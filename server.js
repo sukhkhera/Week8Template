9aa22ac72c45d42d8310d0f6399eb12f849c12d8
sukhkhera <sukh.khera@hotmail.com>
2019-03-27 12:48:31 -0700

    Initial Commit

---
+++ server.js
@@ -0,0 +1,75 @@
+const express = require('express')
+const hbs = require('hbs')
+const fs = require('fs')
+
+var app = express();
+
+hbs.registerPartials(__dirname + '/views/partials');
+
+app.set('view engine', 'hbs');
+app.use(express.static(__dirname + '/public'));
+
+
+hbs.registerHelper('getCurrentYear', () => {
+	return new Date().getFullYear();
+})
+
+hbs.registerHelper('message', (text) => {
+	return text.toUpperCase();
+});
+
+// app.use((request, response, next) => {
+// 		var time = new Date().toString();
+// 		var log = `${time}: ${request.method} ${request.url}`;
+// 		fs.appendFile('server.log', log + '\n', (error) => {
+// 			if (error) {
+// 				console.log('Unable to log message');
+// 			}
+// 		});
+// 		next();
+// });
+
+app.use((request, response) => { 
+	response.render('maintenance.hbs', {
+		log: "The site is currently down for maintenance"
+	});
+		// fs.appendFile('server.log', log + '\n', (error) => {
+		// 	if (error) {
+		// 		console.log('Unable to log message');
+		// 	}
+		// });
+		// next();
+});
+
+
+
+app.get('/', (request, response) => {
+
+	response.send({
+		name: 'Your Name',
+		school: [
+			'BCIT',
+			'SFU',
+			'UBC'
+		]
+	})
+});
+
+app.get('/info', (request, response) => {
+	response.render('about.hbs', {
+		title: 'About page',
+		//year: new Date().getFullYear()
+		welcome: "Hello!"
+	});
+
+});
+
+app.get('/404', (request, response) => {
+	response.send({
+		error: 'Page not found'
+	})
+})
+
+app.listen(8080, () => {
+	console.log('Server is up on the port 8080');
+});
